70fb7db1e000c69e90eef7356f07a3c9fa983b10
Tvisha <tvisha@Tvishas-MacBook-Pro.local>
2025-11-28 18:10:16 -0800

    tvisha changes

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "unit_tests.h"
+
+int main() {
+    test1();
+}
---
+++ processing.h
@@ -0,0 +1,21 @@
+/*
+ *  processing.h
+ *  Henry Dotterer and Tvisha Gupta 
+ *  11/18/25
+ *
+ *  CS 15 PROJ4
+ *
+ *  purpose: This file contains the interface for the processing.cpp functions.
+ */
+
+
+#ifndef __PROCESSING_H
+#define __PROCESSING_H
+
+#include <string>
+using namespace std;
+
+string stripNonAlphaNum(string input);
+void traverseDirectory(string directory);
+
+#endif 
