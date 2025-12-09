744c8bfddc2663d0a5377891abc5452a14717e98
Henry Kodi Dotterer <kdott360@gmail.com>
2025-12-09 13:03:23 -0500

    Add Location struct definition and initial README file

--- gerp/Location.h
+++ gerp/Location.h
@@ -1,3 +1,14 @@
+/*
+ *  Location.h
+ *  Henry Dotterer and Tvisha Gupta 
+ *  11/23/25
+ *
+ *  CS 15 PROJ4
+ *
+ *  purpose: Creates the publically accessible Location struct used to
+ *           store the fileID, lineNum, and originalWord for each word
+ *           instance in the hash table. Also no implementation .cpp file needed
+ */
 #ifndef LOCATION_H
 #define LOCATION_H
 
